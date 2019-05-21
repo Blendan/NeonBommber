840378c9d99253db673cb3e57ad67376ba87074f
Blendan <37484137+Blendan@users.noreply.github.com>
2019-05-21 09:13:20 +0200

    added main menue

---
+++ Assets/scripts/MenueScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenueScript : MonoBehaviour
+{
+    public Button btnStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnStart.onClick.AddListener(StartGame);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("game");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
