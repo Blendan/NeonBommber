58145374f29c3d8d7f3fbc5a7a099c7ca246613f
Blendan <37484137+Blendan@users.noreply.github.com>
2019-06-09 15:08:25 +0200

    addet the ability to mute musik

--- Assets/scripts/MusicPlayer.cs
+++ Assets/scripts/MusicPlayer.cs
@@ -8,29 +8,52 @@ public class MusicPlayer : MonoBehaviour
 
     private AudioSource audioSource;
     private int last = -1;
+    private static bool isMuted = false;
     // Start is called before the first frame update
     void Start()
     {
         audioSource = gameObject.GetComponent<AudioSource>();
 
-        last = (int)Random.Range(0, music.Length - 1);
-        audioSource.clip = music[last];
-        audioSource.Play();
+        if (!isMuted)
+        {
+            last = (int)Random.Range(0, music.Length - 1);
+            audioSource.clip = music[last];
+            audioSource.Play();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!audioSource.isPlaying)
+        if (!audioSource.isPlaying&& !isMuted)
         {
-            int id = -1;
-            while((id = (int)Random.Range(0, music.Length - 1))==last)
-            {
-                //äh ja //TODO ?
-            }
-            audioSource.clip = music[id];
-            audioSource.Play();
-            last = id;
+            PlayAudio();
         }
+
+        if(Input.GetKeyDown(KeyCode.M)||Input.GetKeyDown("joystick button 3"))
+        {
+            isMuted = !isMuted;
+
+            if (isMuted)
+            {
+                audioSource.Stop();
+            }
+            else
+            {
+                PlayAudio();
+            }
+        }
+    }
+
+    private void PlayAudio()
+    {
+        int id = -1;
+        while ((id = (int)Random.Range(0, music.Length - 1)) == last)
+        {
+            //shuld only prodie fi its a different song
+        }
+        audioSource.clip = music[id];
+        audioSource.Play();
+        last = id;
     }
 }
